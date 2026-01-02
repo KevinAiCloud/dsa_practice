ef7828c3b9caad75c4292ce69951b2eb28c218f7
Kevin Christopher Ruban <kevinruban57@gmail.com>
2026-01-02 20:48:00 +0530

    changed file name and updated class name

--- tuf/Java/linked-list/LinkedListCycle.java
+++ tuf/Java/linked-list/LinkedListCycle.java
@@ -1,15 +1,13 @@
-/**
- * Definition for singly-linked list.
- * class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
- * }
- */
-public class Solution {
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
+     }
+ }
+public class LinkedListCycle {
     public boolean hasCycle(ListNode head) {
         if(head==null) 
             return false;
